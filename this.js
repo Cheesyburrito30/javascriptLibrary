50eb0cf89da3f631e491d68580c99c407f46786e
Vernon Perry <pvernon30@gmail.com>
2017-08-31 04:34:51 -0400

    this

---
+++ this.js
@@ -0,0 +1,9 @@
+let Person = function(first, last, age){
+	this.first = first,
+	this.last = last,
+	this.age = age
+}
+//This is pretty easy.
+//'this' is just pointing back at whatever the object that's selected is... without having to type out what the object is.
+// useful for loops, functions, and object constructors.
+
